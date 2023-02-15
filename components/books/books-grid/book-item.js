65314ced3edd9ac837b2d88c13f0766ddb425956
Damien-255 <Damienr@live.co.uk>
2023-02-15 10:59:40 +0000

    added images and a like button for each book

--- components/books/books-grid/book-item.js
+++ components/books/books-grid/book-item.js
@@ -1,8 +1,24 @@
+import { useState } from "react";
+
 export default function BookItem({ book }) {
+    const [likes, setLikes] = useState(0);
+
+    const handleLike =() => {
+        let newLikes = likes + 1;
+        setLikes(newLikes);
+    }
+
     return (
-        <div className="bg-red-100">
-            <p>{book.title}</p>
-            <p>{book.author}</p>
+        <div className="border rounded-lg p-4">
+            <img src={book.imgUrl} className="rounded-lg h-26 "/>
+            <p className="text-xl font-medium">{book.title}</p>
+            <p className="text-gray-700">{book.author}</p>
+            <button
+               className="bg-blue-500 px-2 py-1 rounded text-white"
+            
+               >
+                  like ({likes} likes)
+               </button>
         </div>
     )
 }
--- pages/api/books.js
+++ pages/api/books.js
@@ -2,15 +2,18 @@ export default function handler (req, res) {
 
     const books = [
         { title: "Game of Thrones",
-          author: "George RR Martin",    
+          author: "George RR Martin",
+          imgUrl: "/assets/1.png"    
         },
         {
           title: "Atomic Habits",
           author: "James Clear",
+          imgUrl: "/assets/2.png"
         },
         {
           title: "Born A Crime",
           author: "Trevor Noah",
+          imgUrl: "/assets/3.png"
         },
      
      ];
