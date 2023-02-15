4b0e9fd964418da45d7e316b4d80e3d7aeb1d494
Damien-255 <Damienr@live.co.uk>
2023-02-15 21:02:44 +0000

    added Link section and the onClick handlelike and also updated the const code section

--- components/books/books-grid/book-item.js
+++ components/books/books-grid/book-item.js
@@ -1,24 +1,48 @@
+import Link from "next/link";
 import { useState } from "react";
 
 export default function BookItem({ book }) {
-    const [likes, setLikes] = useState(0);
+    const [likes, setLikes] = useState(+book.likes);
 
-    const handleLike =() => {
+    const handleLike = async () => {
         let newLikes = likes + 1;
         setLikes(newLikes);
+
+        const id = +book.id;
+        const response = await fetch(`/api/increase-likes?id=${id}&likes=${newLikes}`);
+        const data = await response.json();
     }
 
     return (
         <div className="border rounded-lg p-4">
-            <img src={book.imgUrl} className="rounded-lg h-26 "/>
-            <p className="text-xl font-medium">{book.title}</p>
+            <img src={book.img_url} className="rounded-lg h-26" />
+            <Link href={`/single-book/${book.id}`} className="text-x1 font-medium">
+                {book.title}
+            </Link>
             <p className="text-gray-700">{book.author}</p>
             <button
+               onClick={handleLike}
+               type="button"
                className="bg-blue-500 px-2 py-1 rounded text-white"
             
                >
-                  like ({likes} likes)
-               </button>
+                  Like ({likes} likes)
+            </button>
         </div>
     )
 }
+
+
+
+
+/*<div className="border rounded-lg p-4">
+<img src={book.img-url} className="rounded-lg h-26" />
+<p className="text-xl font-medium">{book.title}</p>
+<p className="text-gray-700">{book.author}</p>
+<button
+   className="bg-blue-500 px-2 py-1 rounded text-white"
+
+   >
+      like ({likes} likes)
+   </button>
+</div>*/
