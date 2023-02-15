2c158cc4668e4c0d44694a872a5a30f1744357fd
Damien-255 <Damienr@live.co.uk>
2023-02-15 09:17:27 +0000

    created topics components and added to homepage

---
+++ components/home/topics/topics.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default [
+
+     "React",
+      "State hook",
+      "Effect hook",
+      "Next.js",
+      "Node.js",
+      "Git",
+      "React components",
+      "Mobile responsive design",
+];
